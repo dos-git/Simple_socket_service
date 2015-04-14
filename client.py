cab58b457d5cc41f930df0a5ed3a00fd59193ea4
dos <dos@lx.site>
2015-04-15 01:46:58 +0200

    Working with exception and design the program flow

--- client.py
+++ client.py
@@ -1,25 +1,30 @@
-import sys, os, socket
+import sys, os, socket, time
 
 
 addr = "127.0.0.1"
-port = 1 
+#port = 1 
+port = 4444
 
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print "Socket successfully created"
-except socket.error as err:
-    print "socket creation failed with error %s" %(err)
+while True:
 
-s.connect((addr, port))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        print "Socket fd[%s] successfully created " % s.fileno()
 
-#data = s.recv(1024)
-#print "Received [%s]" % data
-s.send("DOMINO BOSS")
-data = s.recv(1024)
-print "Received [%s]" % data
-s.close()
-print data
+        print "CONNECTING to ip [%s] port [%s] fd[%s]" % (addr, port, s.fileno())
+        s.connect((addr, port))
+        #data = s.recv(1024)
+        #print "Received [%s]" % data
+        s.send("DOMINO BOSS")
+        data = s.recv(1024)
+        print "Received [%s]" % data
+        #s.close()
+        print data
+    except socket.error as err:
+        print "%s [%s]" % (err.strerror, err.errno)
 
-
+    finally:
+        s.close()
+        time.sleep(2)
--- simple_service.py
+++ simple_service.py
@@ -2,27 +2,40 @@ import sys, os, socket
 import time
 
 addr = "127.0.0.1"
-port = 1 
+#port = 1 
+port = 4444 
 
-
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print "Socket successfully created"
-except socket.error as err:
-    print "socket creation failed with error %s" %(err)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+print "Socket successfully created"
 
 s.bind((addr, port))
 print "BINDING...."
 s.listen(1)
-print "LISTENING on ip [%s] port [%s]" % (addr, port)
+print "LISTENING on ip [%s] port [%s] fd[%s]" % (addr, port, s.fileno())
+
 while True:
-    conn,addr = s.accept()
-    print "ACCEPTING...."
 
-    data = conn.recv(1024)
-    print "Received [%s]" % data
-    conn.send("STEFANO BOSS")
-    conn.close()
-    time.sleep(2)
+    try:
 
+        conn,addr = s.accept()
+        print "ACCEPTING.... fd[%s]" % conn.fileno()
+        time.sleep(2)
+        data = conn.recv(1024)
+        print "Received [%s]" % data
+        conn.send("STEFANO BOSS")
+        #conn.close()   # works
+        #s.close()      # error - bad descriptor
+    except socket.error as err:
+        if err.errno == 111:
+            print "111"
+            pass
+
+        print "%s [%s]" % (err.strerror, err.errno)
+#        s.close()
+    except Exception as e:
+        print e.message
+        conn.close()
+    finally:
+        conn.close()
+        time.sleep(2)
